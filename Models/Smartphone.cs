48a0e17cd039a89e9f4ad4bf0ce5ee0cc74cfc54
Felipe Bajerl <felipedpb0607@gmail.com>
2024-09-29 21:32:09 -0300

    Desafio Concluído

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,22 +1,34 @@
+using System.Dynamic;
+
 namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        
+        private string Modelo { get; set; }
 
-        public Smartphone(string numero)
+        private string IMEI { get; set; }
+
+        private int Memoria { get; set; }
+
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
             // TODO: Passar os parâmetros do construtor para as propriedades
         }
 
-        public void Ligar()
+
+        public virtual void Ligar()        
         {
             Console.WriteLine("Ligando...");
-        }
+        }            
 
-        public void ReceberLigacao()
+        public virtual void ReceberLigacao()
         {
             Console.WriteLine("Recebendo ligação...");
         }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("123456", "Modelo 1","1111111111", 256);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone:");
+Smartphone iPhone = new Iphone(numero: "45678", modelo: "Modelo 2", imei: "22222222222", memoria: 128);
+iPhone.Ligar();
+iPhone.InstalarAplicativo("Instagram");
